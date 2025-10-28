52b8c6525eaa062dac159111532267b879c291bb
kh4nhNLK7068 <khanhnguyennlk4198@gmail.com>
2025-10-28 08:32:23 +0700

    Add project files.

---
+++ ClassManagement/Article.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ClassManagement
+{
+    public class Article
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
