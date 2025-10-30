6995d1c3a9ab855cc2cce07cab65815ad9149f0e
kh4nhNLK7068 <khanhnguyennlk4198@gmail.com>
2025-10-30 09:44:52 +0700

    Refactor: Add intial Sign up form

---
+++ ClassManagement/Views/Account/SignUpForm.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ClassManagement.Views.Account
+{
+    public partial class SignUpForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnSignUp_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Text.Trim();
+            string confirm = txtConfirm.Text.Trim();
+
+            if (password != confirm)
+            {
+                lblMessage.Text = "Passwords do not match.";
+                return;
+            }
+
+            string connStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                string query = "INSERT INTO [User](Username, Password, Status) VALUES(@Username, @Password, 1)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Password", password);
+                cmd.ExecuteNonQuery();
+            }
+
+            lblMessage.ForeColor = System.Drawing.Color.Green;
+            lblMessage.Text = "Sign up successful!";
+        }
+    }
+}
